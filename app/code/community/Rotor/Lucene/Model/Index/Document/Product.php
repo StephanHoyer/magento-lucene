ece2bdcfcd30a20daa3e99f5b9363cf9be8929d4
Stephan Hoyer <stephanhoyer@Stephan-Hoyers-MacBook.local>
2010-05-16 20:51:16 +0200

    all searchable attributes are now added to index on building index

--- app/code/community/Rotor/Lucene/Model/Index/Document/Product.php
+++ app/code/community/Rotor/Lucene/Model/Index/Document/Product.php
@@ -3,6 +3,8 @@ class Rotor_Lucene_Model_Index_Document_Product extends Rotor_Lucene_Model_Index
 {
     const DOCTYPE = 'product';
 
+	protected $_systemAttributes = array('name', 'short_content', 'url');
+
     protected function getEntityCollection()
     {
         $collection = Mage::getModel('catalog/product')
@@ -25,18 +27,33 @@ class Rotor_Lucene_Model_Index_Document_Product extends Rotor_Lucene_Model_Index
 
     protected function addAttributes()
     {
-        $this->addField(Zend_Search_Lucene_Field::UnStored('content', 
-			$this->getSourceModel()->getDescription(), self::ENCODING));
         $this->addField(Zend_Search_Lucene_Field::Text('name',
                 $this->getSourceModel()->getName(), self::ENCODING));
 /*
+		$this->addField(Zend_Search_Lucene_Field::UnStored('content', 
+			$this->getSourceModel()->getDescription(), self::ENCODING));
         $this->addField(Zend_Search_Lucene_Field::Keyword('category',
-                $this->getSourceModel()->getParentCategory()->getName(), self::ENCODING));
+            $this->getSourceModel()->getParentCategory()->getName(), self::ENCODING));
 */      
 		$this->addField(Zend_Search_Lucene_Field::UnIndexed('short_content', 
 			$this->getSourceModel()->getShortDescription(), self::ENCODING));
         $this->addField(Zend_Search_Lucene_Field::UnIndexed('url',
-        $this->getSourceModel()->getUrlInStore(), self::ENCODING));
+        	$this->getSourceModel()->getUrl(), self::ENCODING));
+		foreach($this->getSearchableAttributes() as $attribute) {
+			if(in_array($attribute->getAttributeCode(), $this->_systemAttributes)) {
+				continue;
+			}
+			if($this->getSourceModel()->getData($attribute->getAttributeCode())) {
+				$value = $this->getSourceModel()->getAttributeText($attribute->getAttributeCode());
+				if(!$value) {
+					$value = $this->getSourceModel()->getData($attribute->getAttributeCode());
+				}
+				Mage::log($attribute->getAttributeCode().":".$value);
+		        $this->addField(Zend_Search_Lucene_Field::UnStored($attribute->getAttributeCode(),
+		        	$value, self::ENCODING));
+			}
+			
+		}
         /*
 		if($this->getSourceModel()->getImage()) {
             try {
@@ -64,4 +81,16 @@ class Rotor_Lucene_Model_Index_Document_Product extends Rotor_Lucene_Model_Index
         }
         return $this->_entityModel;
     }
+
+	protected function getSearchableAttributes()
+	{
+		if(!isset($this->_searchableAttributes)) {
+			$this->_searchableAttributes = Mage::getModel('eav/entity_attribute')
+				->getCollection()
+				->setEntityTypeFilter($this->getSourceModel()->getEntityTypeId());
+			$this->_searchableAttributes->getSelect()
+				->where('additional_table.is_searchable = ?', 1);
+		}
+		return $this->_searchableAttributes;
+	}
 }
